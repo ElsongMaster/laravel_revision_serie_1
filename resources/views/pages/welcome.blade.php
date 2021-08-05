210fcfcbadc483313c1e9de5fc0dd6886767501a
ElsongMaster <elsongmaster@gmail.com>
2021-08-05 11:28:25 +0200

    Exercice 2 fin

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -6,6 +6,9 @@
 
 
 @section('content')
+@if(is_numeric($var))
 <h3>Resultat de {{$var}}*3 est {{$var*3}}</h3>
-    
+@else
+<h3 style="color: red">La valeur de la variable  recu n'est pas un nombre!</h3>
+@endif    
 @endsection
